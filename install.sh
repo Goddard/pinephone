c57eb4cf1778c5a2085cc0a1f222609074cef40c
Ryein Goddard <ryein@live.com>
2020-02-08 14:33:48 -0500

    updated and all is working if device is not mount

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# include this boilerplate
 function jumpto
 {
     label=$1
@@ -8,54 +7,74 @@ function jumpto
     exit
 }
 
-echo "Checking if Bmap Tool is installed"
-if [ test -x $(which bmaptool) ]; then
-    echo "Bamp Tool is installed"
+echo "Checking if Bmap Tool & Gzip is installed"
+pkgs='bmap-tools gzip dosfstools'
+if ! dpkg -s $pkgs >/dev/null 2>&1; then
+  sudo apt-get install $pkgs
 else
-    echo "Installing bmap-tools from repositories"
-    sudo apt install bmap-tools
+    echo "Packages found"
+    echo ""
 fi
 
 IMAGE_URL=https://ci.ubports.com/job/rootfs/job/rootfs-pinephone/lastSuccessfulBuild/artifact/ubuntu-touch-pinephone.img.gz
 
 wget -N https://ci.ubports.com/job/rootfs/job/rootfs-pinephone/lastSuccessfulBuild/artifact/ubuntu-touch-pinephone.img.bmap
 
-CHECKSUM_TYPE=($(grep -oP '(?<=ChecksumType>)[^<]+' "ubuntu-touch-pinephone.img.bmap"))
-CHECKSUM=($(grep -oP '(?<=BmapFileChecksum>)[^<]+' "ubuntu-touch-pinephone.img.bmap"))
-
-echo "Checksum found : $CHECKSUM"
-echo "Checksum type : $CHECKSUM_TYPE"
-
-FILE=ubuntu-touch-pinephone.img.gz
-IMGFILE=ubuntu-touch-pinephone.img
-
-start=${1:-"start"}
+# if they don't have bmap tool we could do a block level checksum foreach with this?
+# CHECKSUM_TYPE=($(grep -oP '(?<=ChecksumType>)[^<]+' "ubuntu-touch-pinephone.img.bmap"))
+# CHECKSUM=($(grep -oP '(?<=BmapFileChecksum>)[^<]+' "ubuntu-touch-pinephone.img.bmap"))
+# echo "Checksum found : $CHECKSUM"
+# echo "Checksum type : $CHECKSUM_TYPE"
+# TODO : test with dd? this needs work
+# for i in ${!BlockMap[*]}
+# do
+#   echo "$i" "${BlockMap[$i]}"
+# done
+FILE="ubuntu-touch-pinephone.img.gz"
+IMGFILE="ubuntu-touch-pinephone.img"
 
 #start:
 if test -f "$FILE"; then
     echo "Phone Image : $FILE exist"
     #imagetest:
     if test -f "$IMGFILE"; then
-        echo "Phone Image is ungzipped, running checksum"
-        CHECKSUM_RESULT=$($CHECKSUM_TYPE"sum" $IMGFILE)
-        if [[ $CHECKSUM_RESULT = $CHECKSUM ]]; then
-            echo "Checksum is a match, not re-downloading."
-            jumpto end
-        else
-            echo "Checksum doesn't match, re-downloading."
-            wget -N $IMAGE_URL
-            jumpto start
-        fi
+        echo "Phone Image is ungzipped, running mkfs/Bmap Tool"
+        echo ""
+        echo "Please enter your drive path(example:: /dev/sdd ) : "
+        echo " -- MAKE SURE THIS IS THE CORRECT ROOT DEVICE PATH TO YOUR SDCARD --"
+        echo " -- !! IT WILL DESTROY ALL DATA ON THE DEVICE !! --"
+        read DEVICE_PATH
+        echo "Formatting Device"
+        # TODO : check if device is mounted and fix
+        # ls -hl $DEVICE_PATH"*"
+        # echo "Does this look correct? (yes/no)"
+        # echo "If the device cannot be found then it may already be unmounted. Which is fine."
+        # read ANSWER
+        # if [[ $ANSWER="yes" ]]; then
+            # umount $DEVICE_PATH"*"
+            sudo mkfs -t fat $DEVICE_PATH
+            sudo bmaptool copy ubuntu-touch-pinephone.img $DEVICE_PATH
+        # fi
+        # TODO : add checking for dd and bmap?
+        # CHECKSUM_RESULT=$($CHECKSUM_TYPE"sum" $IMGFILE)
+        # if [[ $CHECKSUM_RESULT = $CHECKSUM ]]; then
+        #     echo "Checksum is a match, not re-downloading."
+        #     jumpto end
+        # else
+        #     echo "Checksum doesn't match, re-downloading."
+        #     wget -N $IMAGE_URL
+        #     jumpto start
+        # fi
     else
-        echo "Phone Image not present ungzipping now."
+        echo "Phone Image not present ungzipping now...may take awhile depending on your system speed."
         gzip -d $FILE
         jumpto imagetest
     fi
 else
     echo "Downloading image"
     wget -N $IMAGE_URL
-    jumpto $imagetest
+    jumpto imagetest
 fi
 
 #end:
-echo "this is the end"
+echo "Finished"
